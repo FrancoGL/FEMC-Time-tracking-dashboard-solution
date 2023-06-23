5817cafdfae3552f60c2236a2baf3dc28ddf3d61
Franco Lorca <lorcafrancog@proton.me>
2023-06-23 15:57:06 -0300

    ✨ Feat: Create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CProfileComponent } from './components/cards/c-profile/c-profile.component';
+import { CActivityComponent } from './components/cards/c-activity/c-activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CProfileComponent,
+    CActivityComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/cards/c-activity/c-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c-activity',
+  templateUrl: './c-activity.component.html',
+  styleUrls: ['./c-activity.component.scss']
+})
+export class CActivityComponent {
+
+}
