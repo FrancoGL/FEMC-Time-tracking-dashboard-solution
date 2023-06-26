ca8e6bc8dbd9dc6055a841590ca2eed3ee797335
Franco Lorca <lorcafrancog@proton.me>
2023-06-26 16:47:57 -0300

    📝 Update(c-activity.component.ts): Implement ActivityService

--- src/app/components/cards/c-activity/c-activity.component.ts
+++ src/app/components/cards/c-activity/c-activity.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivityService} from "../../../common/services/activity.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-c-activity',
   templateUrl: './c-activity.component.html',
   styleUrls: ['./c-activity.component.scss']
 })
-export class CActivityComponent {
+export class CActivityComponent implements OnInit, OnDestroy{
 
+  subscription!: Subscription;
+  activities!: any;
+  currentTimeFrame: string = 'Daily'
+  timeFrameData: any;
+
+  constructor(private activityService: ActivityService) {
+  }
+
+  ngOnInit() {
+    this.subscription = this.activityService.getAll().subscribe((res) => {
+      this.activities = res
+      console.log(res)
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
 }
