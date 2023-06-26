02bd26421d65c37cb144c437d8f94642d396477c
Franco Lorca <lorcafrancog@proton.me>
2023-06-26 16:46:51 -0300

    📝 Update(activity.service.ts): Create method get

--- src/app/common/services/activity.service.ts
+++ src/app/common/services/activity.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
 
-  constructor() { }
+  dataPath: string = "../../assets/data/data.json"
+
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get(this.dataPath)
+  }
 }
